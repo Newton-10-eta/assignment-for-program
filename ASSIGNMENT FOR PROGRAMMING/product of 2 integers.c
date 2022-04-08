9e8b6572b963f2d3012e84bc68297d9a2ca2603d
Newton-10-eta <103262781+Newton-10-eta@users.noreply.github.com>
2022-04-08 16:56:47 +0300

    Add files via upload

---
+++ ASSIGNMENT FOR PROGRAMMING/product of 2 integers.c
@@ -0,0 +1,10 @@
+//programme to find the product of two integers
+#include <stdio.h>
+int main()
+{  
+     int i=5,r=8,k;
+     k=i*r
+     printf("product of two integers %d",k);
+
+    return 0;
+}
---
+++ ASSIGNMENT FOR PROGRAMMING/sum of 3 nos.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int i=7,k=9,t=10,r;
+    r=i+k+t;
+    printf("sum of three numbers is %d",r);
+    return 0;
+
+}
